06dfd59df0c1bd13b1e51e9d48b402a20dac8047
unknown <jobu12@H429-0011.students.ad.bth.se>
2014-04-15 16:19:08 +0200

    Maploaders h och cpp

--- LillaSpelTest/Include/MapLoader.h
+++ LillaSpelTest/Include/MapLoader.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <DirectXMath.h>
+#include <vector>
+#include <d3d11_1.h>
+#include "MapNode.h"
+
+using namespace std;
+using namespace DirectX;
+
 class MapLoader
 {
 public:
 	MapLoader(void);
+	vector<MapNode*>LoadMap(string mapName);
 	~MapLoader(void);
+private:
+	vector<vector<XMFLOAT3>> LoadLogicalObj(string splineName);
+	//vector<StaticObj*> LoadBoxes(string boxListName);
+
 };
 
