a5e2872575c700326c9d139d109897ac1621d8bc
unknown <luho12@H429-0012.students.ad.bth.se>
2014-04-17 16:14:02 +0200

    Fortsatt med physics och mathhelper

--- LillaSpelTest/Source/PhysicsManager.cpp
+++ LillaSpelTest/Source/PhysicsManager.cpp
@@ -16,14 +16,14 @@ PhysicsManager::~PhysicsManager(void)
 
 std::vector<DirectX::XMFLOAT3> PhysicsManager::GetForceInCollision(Player p_p1, Player p_p2)
 {
+	MathHelper t_mHelp = MathHelper();
 	std::vector<DirectX::XMFLOAT3> r_return;
 
 	XMFLOAT3 t_p1Pos = p_p1.GetPos();
-	XMFLOAT3 t_p2Pos = p_p2.GetPos();
 	XMFLOAT3 t_collisionVec;
 
-	XMFLOAT3 t_p1Direction = p_p1.GetDirection();
-	XMFLOAT3 t_p2Direction = p_p1.GetDirection();
+	float t_p1DirSkal = t_mHelp.SkalarProdukt(p_p1.GetDirection(), t_collisionVec);
+	float t_collisionSkal = t_mHelp.SkalarProdukt(t_collisionVec, t_collisionVec);
 
 	XMFLOAT3 t_p1Projection;
 	XMFLOAT3 t_p2Projection;
@@ -31,7 +31,7 @@ std::vector<DirectX::XMFLOAT3> PhysicsManager::GetForceInCollision(Player p_p1, 
 	t_collisionVec = XMFLOAT3(t_p1Pos.x-t_p2Pos.x, t_p1Pos.y - t_p2Pos.y, t_p1Pos.z - t_p2Pos.z); //Vi ska projecera på denna vector
 	
 	//Skalär av vectorn som ska projeceras på collisionvec
-	t_p1Projection = ((
+	t_p1Projection = (t_p1DirSkal / t_collisionSkal)*t_collisionVec;
 
 
 
