34b77db9d1e8612b657e6a7e49a6b5e107915d6f
unknown <kooh12@H429-0015.students.ad.bth.se>
2014-04-25 14:38:58 +0200

    Fixed some menues

--- LillaSpelTest/Include/MysteriskTest.h
+++ LillaSpelTest/Include/MysteriskTest.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <vector>
 #include "UserCMD.h"
+#include "GraphicHandle.h"
 
 class MysteriskTest
 {
 public:
 	MysteriskTest(void);
+	MysteriskTest(GraphicHandle* grphandle);
 	~MysteriskTest(void);
 	void Run(std::vector<UserCMD>* players, float dt);
 };
