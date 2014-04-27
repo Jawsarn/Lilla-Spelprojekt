1e2c220f65715aadb8ee489def828a7e5d925b1d
unknown <konrad_o_92@hotmail.com>
2014-04-27 17:08:05 +0200

    added fmod dlls libs and h files

--- LillaSpelTest/Source/OptionsScreen.cpp
+++ LillaSpelTest/Source/OptionsScreen.cpp
@@ -6,12 +6,53 @@ OptionsScreen::OptionsScreen(void)
 }
 
 OptionsScreen::OptionsScreen(GameInfo* p_gameInfo, GraphicHandle* p_graphicsHandle)
+	:MenuScreen(p_gameInfo,p_graphicsHandle)
 {
 	AddButton("Volume", DirectX::XMFLOAT2(0,0.8),0.7,0.1);
-	AddButton("Volume", DirectX::XMFLOAT2(0,0.8),0.7,0.1);
+	AddButton("Resolution", DirectX::XMFLOAT2(0,0.8),0.5,0.1);
+	AddButton("Back", DirectX::XMFLOAT2(0,0.8),0.7,0.1);
+	FixButtonPointers();
 }
 
 
 OptionsScreen::~OptionsScreen(void)
 {
+}
+
+int OptionsScreen::Update(std::vector<UserCMD>* userCMD, float p_dt)
+{
+	MenuScreen::Update( userCMD, p_dt);
+	std::string t_menuChoice = NavigateMenu(userCMD->at(0));
+
+	if (currentButton->buttonName == "Volume")
+	{
+		AlterVolume(userCMD->at(0));
+	}
+	else if (t_menuChoice == "Resolution")
+	{
+
+	}
+	else if (t_menuChoice == "Back" || userCMD->at(0).backButtonPressed)
+	{
+		return MAIN_MENU_SCREEN;
+	}
+
+	return OPTIONS_SCREEN;
+}
+
+void OptionsScreen::AlterVolume(UserCMD& p_userCMD)
+{
+	if (timeSinceLastChange[0] <0.03)
+	{
+		if (p_userCMD.Joystick.x<-0.8 && m_volume > 0)
+		{
+			m_volume -= VOLUME_CHANGE;
+			timeSinceLastChange[0]=0;
+		} 
+		else if (p_userCMD.Joystick.x>0.8 && m_volume < MAX_VOLUME)
+		{
+			m_volume += VOLUME_CHANGE;
+			timeSinceLastChange[0]=0;
+		}
+	}
 }
