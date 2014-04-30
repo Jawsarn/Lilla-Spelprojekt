41a61070b2201696ccc56bef06687c4d33165224
Yooric <yooric90@hotmail.com>
2014-04-30 11:43:54 +0200

    started fixing the red thread making all have the menu and clicking through it

--- LillaSpelTest/Source/GraphicHandle.cpp
+++ LillaSpelTest/Source/GraphicHandle.cpp
@@ -152,7 +152,7 @@ void GraphicHandle::Initialize(UINT p_Width, UINT p_Height, HWND p_Handle)
 	//VilkenVehicle.push_back(0);
 	//VilkenVehicle.push_back(0);
 	//t_Mat = XMMatrixTranslation(10,10,10); 
-	StartGame(0,VilkenVehicle,plajerwurld,plajercullur,t_World,t_Color);
+	//StartGame(0,VilkenVehicle,plajerwurld,plajercullur,t_World,t_Color);
 	
 	
 	
@@ -254,7 +254,7 @@ void GraphicHandle::Initialize(UINT p_Width, UINT p_Height, HWND p_Handle)
 
 
 
-	UpdatePlayer(0,t_Mat);
+//	UpdatePlayer(0,t_Mat);
 }
 
 void GraphicHandle::UpdatePlayer(int p_playerID,CXMMATRIX p_matrix)
