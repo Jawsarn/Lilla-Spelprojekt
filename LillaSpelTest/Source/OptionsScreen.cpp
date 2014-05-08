a6925da73a02ec92224399c4b78badc29d3d111a
Joel Begnert <joel.begnert@gmail.com>
2014-05-08 14:35:25 +0200

    fixed volume controll

--- LillaSpelTest/Source/OptionsScreen.cpp
+++ LillaSpelTest/Source/OptionsScreen.cpp
@@ -8,18 +8,43 @@ OptionsScreen::OptionsScreen(void)
 OptionsScreen::OptionsScreen(GameInfo* p_gameInfo, GraphicHandle* p_graphicsHandle, AudioManager* p_audioManager)
 	:MenuScreen(p_gameInfo,p_graphicsHandle,  p_audioManager)
 {
-	AddButton("Volume", DirectX::XMFLOAT2(0,0.6),0.7,0.1, L"Volume1.dds",L"Volume2.dds");
+	AddButton("Volume", DirectX::XMFLOAT2(0,0.6),0.5,0.1, L"Volume1.dds",L"Volume2.dds");
 	AddButton("Resolution", DirectX::XMFLOAT2(0,0.2),0.5,0.1,L"Resolution1.dds",L"Resolution2.dds");
 	AddButton("Full screen",DirectX::XMFLOAT2(0,-0.2),0.5,0.1,L"FullScreen1.dds",L"FullScreen2.dds");
 	AddButton("Back", DirectX::XMFLOAT2(0,-0.6),0.5,0.1,L"Back1.dds",L"Back2.dds");
+	std::vector<UINT> t_volumeSlideTextures;
+	UINT t_volumeSlideTexture;
+	UINT t_volumeSlideHandle;
+	m_graphicHandle->LoadTexture(L"VolumeSlide.dds",t_volumeSlideTexture);
+	t_volumeSlideTextures.push_back(t_volumeSlideTexture);
+	m_graphicHandle->CreateHUDObject(DirectX::XMFLOAT2(0,0.8),DirectX::XMFLOAT2(0.5,0.05),t_volumeSlideTextures,t_volumeSlideHandle);
+
 
 	FixButtonPointers();
-	MakeHud(m_hudHandle);
+	//MakeHud(m_hudHandle);
+
+	std::vector<unsigned int> t_buttonHandles;
+	std::vector<DirectX::XMFLOAT2> t_barOffsets;
+
+	for (int i = 0; i < buttonList.size(); i++)
+	{
+		t_buttonHandles.push_back(buttonList[i]->buttonHandle);
+		t_barOffsets.push_back(DirectX::XMFLOAT2(0,0));
+		buttonList[i]->buttonHandle = i;
+	}
+	t_buttonHandles.push_back(t_volumeSlideHandle);
+	t_barOffsets.push_back(DirectX::XMFLOAT2(0,0));
+	m_volumeSlideHandle = t_barOffsets.size() - 1;
+
+	unsigned int t_templateHandle;
+	m_graphicHandle->CreateHudTemplate(t_buttonHandles,t_templateHandle);
+	m_graphicHandle->CreateHudFromTemplate(t_templateHandle,5,t_barOffsets,m_hudHandle);
 
 	//m_graphicHandle->UseHud(0,m_hudHandle);
 	currentButton = buttonList[0];
 	m_graphicHandle->ChangeHudObjectTexture(m_hudHandle,currentButton->buttonHandle,false);
 	m_fullScreen=false;
+	m_volume = 1;
 }
 
 
@@ -77,18 +102,23 @@ void OptionsScreen::Initialize()
 
 void OptionsScreen::AlterVolume(UserCMD& p_userCMD)
 {
-	if (timeSinceLastChange[0] <0.03)
+	if (timeSinceLastChange[0] >0.03)
 	{
 		if (p_userCMD.Joystick.x<-0.8 && m_volume > 0)
 		{
 			m_volume -= VOLUME_CHANGE;
 			timeSinceLastChange[0]=0;
+			m_audioManager->SetMasterVolume(m_volume);
+			m_graphicHandle->UpdateHudBarOffset(m_hudHandle,m_volumeSlideHandle,DirectX::XMFLOAT2(1-m_volume,0));
 		} 
 		else if (p_userCMD.Joystick.x>0.8 && m_volume < MAX_VOLUME)
 		{
 			m_volume += VOLUME_CHANGE;
 			timeSinceLastChange[0]=0;
+			m_audioManager->SetMasterVolume(m_volume);
+			m_graphicHandle->UpdateHudBarOffset(m_hudHandle,m_volumeSlideHandle,DirectX::XMFLOAT2(1-m_volume,0));
 		}
+		
 	}
 }
 
