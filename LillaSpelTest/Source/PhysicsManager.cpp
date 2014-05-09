f1a3e4cdd3465004896810dcd7e93e751bb15572
Erik <erik_the_great_one@hotmail.com>
2014-05-09 13:03:33 +0200

    Halvfärdig physmanager, commit för att pulla azookas changes

--- LillaSpelTest/Source/PhysicsManager.cpp
+++ LillaSpelTest/Source/PhysicsManager.cpp
@@ -42,9 +42,36 @@ std::vector<DirectX::XMFLOAT3> PhysicsManager::GetForceInCollision(Player* p_p1,
 	t_p2Projection.x = -t_p2Projection.x; t_p2Projection.y = -t_p2Projection.y;  t_p2Projection.z = -t_p2Projection.z; //vänder på ena vectorn
 
 	float t_magnitude = t_mHelp.Abs(t_mHelp.VecSubVec(t_p1Projection, t_p2Projection));
-	r_return.push_back(t_mHelp.FloatMultiVec(t_magnitude, t_mHelp.Normalize(t_mHelp.VecSubVec(t_p1Pos, t_p2Pos))));
-	r_return.push_back(t_mHelp.FloatMultiVec(t_magnitude, t_mHelp.Normalize(t_mHelp.VecSubVec(t_p2Pos, t_p2Pos))));
+	XMFLOAT3 t_p1Magnitude = XMFLOAT3 (t_mHelp.FloatMultiVec(t_magnitude, t_mHelp.Normalize(t_mHelp.VecSubVec(t_p1Pos, t_p2Pos))));
+	XMFLOAT3 t_p2Magnitude = XMFLOAT3 (t_mHelp.FloatMultiVec(t_magnitude, t_mHelp.Normalize(t_mHelp.VecSubVec(t_p2Pos, t_p2Pos))));
 	
+
+	///////Här börjar projicering på playersens dirvectorer!
+	XMFLOAT3 t_p1Direction = p_p1->GetDirection();
+	XMFLOAT3 t_p2Direction = p_p2->GetDirection();
+
+	t_p1DirSkal = t_mHelp.DotProduct(t_p1Magnitude,t_p1Direction);
+	t_p2DirSkal = t_mHelp.DotProduct(t_p2Magnitude,t_p2Direction);
+	//Första projeceringen... För player 1
+	float t_p1ProjectionDivider = t_mHelp.DotProduct(t_p1Direction, t_p1Direction); //abs det som ska vara under bråksträcket i proj formeln
+	t_p1ProjectionDivider *= t_p1ProjectionDivider; // ABs ^2
+
+	XMFLOAT3 t_p1DirectionProjection = t_mHelp.FloatMultiVec((t_p1DirSkal / t_p1ProjectionDivider), t_p1Direction); //Klar projecering av kraftvektor på p1s direction.. Den här ska översättas till m_speed
+	///////
+	//p_p1->SetSpeed( t_mHelp.DotProduct(t_p1DirectionProjection, t_p1DirectionProjection);
+	//Första projeceringen.. Förplayer 2
+	float t_p2ProjectionDivider = t_mHelp.DotProduct(t_p2Direction, t_p2Direction); //abs det som ska vara under bråksträcket i proj formeln
+	t_p2ProjectionDivider *= t_p2ProjectionDivider; // ABs ^2
+	XMFLOAT3 t_p2DirectionProjection = t_mHelp.FloatMultiVec((t_p1DirSkal / t_p2ProjectionDivider), t_p2Direction); //Klar Projecering av kraftvektor på p2s direction samma som för p1 ^^^^ där uppe
+	//////////////////////////////////////////////////////////////////////////
+	//p_p2->SetSpeed( t_mHelp.DotProduct(t_p2DirectionProjection, t_p2DirectionProjection);
+
+	//////Projecering på Radiusvectorn!!!!! används för angle påplusning
+	//XMFLOAT3 t_p1Radius = p_p1->GetRadius();
+	//XMFLOAT3 t_p2Radius = p_p2->GetRadius();
+
+
+
 
 
 	return r_return;
