21783dccdd0c47382c8ef6c4bac0282c80878081
Jaws <christian_markowicz@hotmail.com>
2014-05-27 01:43:09 +0200

    fixed shaders

--- LillaSpelTest/Shaders/GraphicInstancedVS.hlsl
+++ LillaSpelTest/Shaders/GraphicInstancedVS.hlsl
@@ -7,8 +7,25 @@ struct VS_INPUT
 	float2 Tex		: TEXCOORD;
 };
 
-
-VS_INPUT VS( VS_INPUT input )
+struct VS_INSTANCED
 {
-	return input;
+	matrix WorldMatrix : INSTANCED;
+};
+
+struct VS_OUTPUT
+{
+	float4 Position : POSITION;
+	float3 Normal	: NORMAL;
+	float2 Tex		: TEXCOORD;
+};
+
+VS_OUTPUT VS( VS_INPUT input, VS_INSTANCED inputInstance )
+{
+	VS_OUTPUT output;
+
+	output.Position = mul(float4(input.Position, 1), inputInstance.WorldMatrix);
+	output.Normal = input.Normal;
+	output.Tex = input.Tex;
+
+	return output;
 }
